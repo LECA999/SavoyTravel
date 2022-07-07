afbeeb0bab24be7e1c3b2b85d872dfe67c647cb8
LECA999 <devoloper.leca@gmail.com>
2022-07-06 20:26:55 -0500

    listo

---
+++ script.js
@@ -0,0 +1,23 @@
+//funcion que me aplica el estilo a la opcion seleccionada en ele menu quita la previamente seleccionado
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0] .className = "";
+    opciones[1] .className = "";
+    opciones[2] .className = "";
+    opciones[3] .className = "";
+    opciones[4] .className = "";
+    link.className = "seleccionado";
+
+    //hacemos desaparecer le meni uan vez que se ha seleccionado uan opcion en el modo respondive 
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+//funcion que muestra el menu responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
